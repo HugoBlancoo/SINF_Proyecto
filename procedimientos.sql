c8d5b17eaa2140b77496541efdee310f720cc731
bedrix19 <146009176+bedrix19@users.noreply.github.com>
2024-05-08 15:49:48 +0200

    Update procedimientos.sql

--- procedimientos.sql
+++ procedimientos.sql
@@ -9,9 +9,44 @@ BEGIN
 END //
 DELIMITER ;
 
-CALL 
+CALL restringir_usuarios();
 
 -- [4] Que no superen los aforos establecidos
+DELIMITER //
+CREATE PROCEDURE 
+BEGIN
+    SELECT
+END //
+DELIMITER ;
+
+CALL 
+
 -- [5] Limitar el numero de entradas/usuarios para un evento
+DELIMITER //
+CREATE PROCEDURE limitar_usuarios()
+BEGIN
+    SELECT
+END //
+DELIMITER ;
+
+CALL limitar_usuarios()
+
 -- [6] Solo vender entradas de eventos abiertos
+DELIMITER //
+CREATE PROCEDURE vender_entrada()
+BEGIN
+    SELECT
+END //
+DELIMITER ;
+
+CALL vender_entrada()
+
 -- [8] No anular una reserva no hecha por mi
+DELIMITER //
+CREATE PROCEDURE anular_reserva()
+BEGIN
+    SELECT
+END //
+DELIMITER ;
+
+CALL 
