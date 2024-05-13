4c32c94f910b2d71c35a6faf6a9da20aff0f62cf
DiegoPerezMartinez <127452152+DiegoPerezMartinez@users.noreply.github.com>
2024-05-13 10:42:35 +0200

    llenar la tabla Usuarios

---
+++ Usuarios.sql
@@ -0,0 +1,4 @@
+INSERT INTO Usuarios (tipo) VALUES ('Infantil');
+INSERT INTO Usuarios (tipo) VALUES ('Jubilado');
+INSERT INTO Usuarios (tipo) VALUES ('Adulto');
+INSERT INTO Usuarios (tipo) VALUES ('Parado');
