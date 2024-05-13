8833f33c705d830990985d55d00d596a6047cbc8
HugoBlancoo <hugoblancodemelo@gmail.com>
2024-05-13 20:22:08 +0200

    Create Realiza.sql

---
+++ Realiza.sql
@@ -0,0 +1,10 @@
+INSERT INTO Realiza (EspectaculoTitulo, EspectaculoTipo, EspectaculoProductor, RecintoNombre, RecintoFecha)
+SELECT 
+    e.Titulo,
+    e.Tipo,
+    e.Productor,
+    r.Nombre,
+    r.Fecha
+FROM 
+    Espectaculo e
+    CROSS JOIN Recinto r;
