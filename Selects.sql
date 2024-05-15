b58d51ad9d4569381ded8ad074eed85f253c8e8d
DiegoPerezMartinez <127452152+DiegoPerezMartinez@users.noreply.github.com>
2024-05-15 13:15:02 +0200

    Select de Vista

--- Selects.sql
+++ Selects.sql
@@ -1,13 +1,17 @@
 SELECT * FROM Cliente;
 SELECT * FROM Espectaculo;
-SELECT * FROM  Recinto WHERE Fecha = '2024-01-10 00:00:00';
+SELECT * FROM  Recinto ;
 SELECT * FROM Localidades;
 SELECT * FROM Usuarios;
 SELECT * FROM  Ofertas;
-SELECT * FROM  Ofertas WHERE UsuarioTipo = 'Infantil' AND LocalidadGrada = 'Grada1';
+SELECT * FROM  Ofertas WHERE UsuarioTipo = 'Infantil';
 SELECT * FROM  Realiza;
 SELECT * FROM  Realiza WHERE RecintoFecha = '2024-01-10 00:00:00';
 SELECT * FROM Permite;
-SELECT * FROM  Pertenecen WHERE R_EspectaculoTitulo = 'Annie' AND OfertaUsuarioTipo = 'Infantil';
+SELECT * FROM  Pertenecen;
 SELECT * FROM Venta;
 DELETE FROM Permite WHERE EspectaculoTitulo = 'Annie' AND UsuarioTipo = 'Parado';
+
+SELECT * FROM vista_Usuario;
+select * FROM vista_Usuario Where R_EspectaculoTitulo = 'Avatar' AND OfertaUsuarioTipo = 'Adulto' AND R_RecintoNombre = 'Recinto1';
+
